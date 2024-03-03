8f1b586117b31ca41268d5c0be6144c8e265f94c
alona <aloyna.vasilieva@gmail.com>
2024-03-03 21:30:21 +0100

    function filter map

--- JAVASCRIPT/6_function/script.js
+++ JAVASCRIPT/6_function/script.js
@@ -32,14 +32,130 @@
 // showText("John");
 // showText("Anna");
 
-function multiply(a, b) {
-let result = a * b;
-console.log("Result", result);
-   return result 
-}
-// multiply(5, 4);
+// function multiply(a, b) {
+// // let result = a * b;
+// // console.log("Result", result);
+// //    return result 
 
-let x = multiply(5, 4)
-if (x) {
-    console.log("x", x);
-}
+// return a * b // правильнее сделать так
+// }
+// // multiply(5, 4);
+
+// let x = multiply(5, 4)
+// if (x) {
+//     console.log("x", x);
+// }
+
+// let add = function (a, b) {
+//     return a + b;
+// }
+// console.log(add(5, 3));
+
+// let add = (a, b) => {
+//     return a + b;
+// }
+// console.log(add(3, 2));
+
+// let add = (a, b) => a + b
+// console.log(add(5, 2));
+
+// let add = x => `Hello ${x}`
+// console.log(add(10));
+
+// let getMax = (a, b) => a > b  ? a : b;
+// console.log(getMax(6, 9));
+
+// let getMax = (a, b,) => a > b  ? a : b;
+// console.log(getMax(getMax(30, 9), getMax (1, 10)));
+
+// function getNumbers(a, b, c) {
+//     console.log(a, b, c);
+    
+// }
+// getNumbers(5, 6, 7);
+
+// function getNumbers() {
+//     console.log(arguments[0], arguments[1], arguments[2]);    
+// }
+// getNumbers(5, 6, 7);
+
+
+// function getNumbers(...arguments) { //spredoperator
+//         console.log(arguments);    
+//     }
+//     getNumbers(5, 6, 7);
+
+// function externalFunc() {  // замыкание - вопрос на собеседовании
+//     let externalVal = "This is external function!"
+//     return function internal() {
+//     let internalVal = "This is internal function!"
+      
+//     console.log("externalVal--->", externalVal);
+//     console.log("internalVal--->", internalVal);      
+//     }
+// }
+// let internalFunc = externalFunc();
+// internalFunc();
+
+// function counter() {  // замыкание - вопрос на собеседовании
+//         let value = 0
+//         return function increment() {
+//             return value++;
+               
+//         }
+//     }
+//     let count = counter();
+//     console.log(count());
+//     console.log(count());
+//     console.log(count());
+
+//  -------------------MAP-----------------
+
+// let cars = ["BMW", "Mercedes", "Audi", "Opel"];
+
+// let modifidedcars = cars.map(function(car, index) {
+//     return car.toUpperCase();
+//     // console.log(car, index);
+    
+    
+// })
+// console.log(cars, modifidedcars);
+
+// let numbers = [1, 3, 4, 2, 7];
+
+// ---------1 вариант-------------
+// let squareNumbers = numbers.map(function (num) {
+//     return num * num;    
+// })
+
+// ---------2 вариант----------------
+// let squareNumbers = numbers.map(el => el**2)
+
+// console.log(squareNumbers);
+
+// --------3 вариант--------------------
+
+// let getSquare = el => el**2;
+// let squareNumbers = numbers.map(getSquare)
+// console.log(squareNumbers);
+
+
+// let numbers = [1, 3, 4, 2, 7];
+// function customMap(arr, callback) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+    
+//     newArr.push(callback(arr[i]));
+//   }  
+//   return newArr
+// }
+
+// console.log(customMap(numbers, el => el**2));
+
+let cars = ["BMW", "Mercedes", "Audi", "Opel"];
+// let carFilter = cars.filter(car => car[0] === "A");
+// let carFilter = cars.filter(car => car.indexOf('e') !== -1);
+let carFilter = cars.filter(car => car.toLowerCase().indexOf('a') !== -1);
+
+
+console.log(carFilter);
